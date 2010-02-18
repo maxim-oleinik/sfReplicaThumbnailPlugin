637659221d86f0630a8af80fdb022bfad9ea1f71
Maxim Oleinik <maxim.oleinik@gmail.com>
2010-02-18 17:08:46 +0300

    Config: Exclude test classes from autoload

---
+++ config/sfReplicaThumbnailPluginConfiguration.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * sfReplicaThumbnailPluginConfiguration
+ */
+class sfReplicaThumbnailPluginConfiguration extends sfPluginConfiguration
+{
+    /**
+     * Initialize
+     *
+     * @see sfPluginConfiguration
+     */
+    public function initialize()
+    {
+        $dir = sfConfig::get('sf_web_dir') . DIRECTORY_SEPARATOR
+             . ltrim(sfConfig::get('app_thumbnail_dir'), '\\/');
+        Replica::setCacheManager(new Replica_Macro_CacheManager($dir));
+    }
+
+
+    /**
+     * Exclude test classes from autoload
+     */
+    public function filterAutoloadConfig(sfEvent $event, array $config)
+    {
+        $config = parent::filterAutoloadConfig($event, $config);
+
+        $libConfig =& $config['autoload'][$this->name.'_lib'];
+        $libConfig['exclude'] = array('test');
+
+        return $config;
+    }
+
+}
